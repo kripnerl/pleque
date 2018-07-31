464dff7cded7281a18555d6533b67243359609f5
Lukas Kripner <kripner@ipp.cas.cz>
2018-07-31 11:02:31 +0200

    add: testing plot of monkey patched functions.

--- test/simple_test.py
+++ test/simple_test.py
@@ -5,10 +5,65 @@ import numpy as np
 import xarray as xa
 
 from pleque.core import Equilibrium
+from pleque.io.test_gfile_loader import read_gfile as test_read_gfile
 
 modpath = os.path.expanduser("/compass/home/kripner/Projects/pyTokamak.git")
 if not modpath in sys.path:  # not to stack same paths continuously if it is already there
     sys.path.insert(0, modpath)
+
+
+def test_qprofiles(g_file: str, eq: Equilibrium):
+    from tokamak.formats import geqdsk
+    import matplotlib.pyplot as plt
+
+    eq_gfile = geqdsk.read(g_file)
+
+    qpsi = eq_gfile['qpsi']
+    psi_N = np.linspace(0, 1, len(qpsi))
+
+    print(eq_gfile.keys())
+
+    psin_axis = np.linspace(0, 1, 100)
+    r = np.linspace(eq.r_min, eq.r_max, 100)
+    z = np.linspace(eq.z_min, eq.z_max, 120)
+    psi = eq.psi(R=r, Z=z)
+
+    plt.figure()
+    plt.subplot(121)
+    ax = plt.gca()
+
+    ax.contour(r, z, psi.T, 30)
+    ax.plot(eq._lcfs[:, 0], eq._lcfs[:, 1], label='lcfs')
+    ax.plot(eq._mg_axis[0], eq._mg_axis[1], 'o', color='b')
+    ax.plot(eq._x_point[0], eq._x_point[1], 'x', color='r')
+    ax.plot(eq._x_point2[0], eq._x_point2[1], 'x', color='r')
+    ax.set_xlabel('R [m]')
+    ax.set_ylabel('Z [m]')
+    ax.set_aspect('equal')
+    ax.set_title(r'$\psi$')
+
+    psi_mod = eq.psi(psi_N=psin_axis)
+    tor_flux = eq.tor_flux(psi_N=psin_axis)
+    q_as_grad = np.gradient(tor_flux, psi_mod)
+
+    plt.subplot(222)
+    ax = plt.gca()
+    ax.plot(psi_N, qpsi, 'x', label='g-file data')
+    ax.plot(psin_axis, q_as_grad, '-',
+            label=r'$\mathrm{d} \Phi/\mathrm{d} \psi$')
+    ax.plot(psin_axis, q_as_grad, '--', label='Equilibrium data')
+
+    ax.legend()
+    ax.set_xlabel(r'$\psi_\mathrm{N}$')
+    ax.set_ylabel(r'$q$')
+
+    plt.subplot(224)
+    ax = plt.gca()
+    ax.plot(psi_mod, tor_flux, label='Toroidal flux')
+    ax.set_xlabel(r'$\psi$')
+    ax.set_ylabel(r'$\Phi$')
+
+
 
 
 def load_gfile(g_file):
@@ -19,9 +74,18 @@ def load_gfile(g_file):
     r = eq_gfile['r'][:, 0]
     z = eq_gfile['z'][0, :]
 
-    eq_ds = xa.Dataset({'psi': (['Z', 'R'], psi)},
+    pressure = eq_gfile['pressure']
+    fpol = eq_gfile['fpol']
+    psi_N = np.linspace(0, 1, len(fpol))
+
+    eq_ds = xa.Dataset({'psi': (['Z', 'R'], psi),
+                        'pressure': ('psi_N', pressure),
+                        'fpol': ('psi_N', fpol)},
                        coords={'R': r,
-                               'Z': z})
+                               'Z': z,
+                               'psi_N': psi_N})
+
+    print(eq_ds)
 
     if False:
         import matplotlib.pyplot as plt
@@ -35,8 +99,8 @@ def load_gfile(g_file):
 def plot_over_view_2d(eq: Equilibrium):
     import matplotlib.pyplot as plt
 
-    r = eq._basedata.R.data
-    z = eq._basedata.Z.data
+    r = np.linspace(eq.r_min, eq.r_max, 100)
+    z = np.linspace(eq.z_min, eq.z_max, 120)
 
     psi = eq.psi(R=r, Z=z)
 
@@ -46,7 +110,7 @@ def plot_over_view_2d(eq: Equilibrium):
 
     plt.figure(figsize=(8, 4))
     plt.subplot(131)
-    plt.pcolormesh(r, z, psi.T)
+    plt.contour(r, z, psi.T, 30)
     plt.plot(eq._lcfs[:, 0], eq._lcfs[:, 1], label='lcfs')
     plt.plot(eq._mg_axis[0], eq._mg_axis[1], 'o', color='b')
     plt.plot(eq._x_point[0], eq._x_point[1], 'x', color='r')
@@ -56,14 +120,15 @@ def plot_over_view_2d(eq: Equilibrium):
     plt.gca().set_aspect('equal')
 
     plt.subplot(132)
-    plt.pcolormesh(eq._basedata.R.data, eq._basedata.Z.data, eq._basedata.psi.data)
-    plt.title(r'$\psi$, original data')
+    plt.contour(r, z, eq.B_pol(R=r, Z=z).T, 30)
+    plt.plot(eq._lcfs[:, 0], eq._lcfs[:, 1], label='lcfs')
+    plt.title(r'$B_\mathrm{pol}$')
     plt.gca().set_aspect('equal')
 
     plt.subplot(133)
-    rgrid, zgrid = np.meshgrid(r, z)
-    plt.pcolormesh(rgrid, zgrid, psi.T)
-    plt.title(r'$\psi$ shown on grid coordinates')
+    plt.contour(r, z, eq.B_tor(R=r, Z=z).T, 30)
+    plt.plot(eq._lcfs[:, 0], eq._lcfs[:, 1], label='lcfs')
+    plt.title(r'$B_\mathrm{tor}$')
     plt.gca().set_aspect('equal')
 
     plt.figure(figsize=(8, 4))
@@ -82,11 +147,33 @@ def plot_over_view_2d(eq: Equilibrium):
     plt.title(r'$B_\mathrm{pol}$')
     plt.gca().set_aspect('equal')
 
-    plt.show()
+    psi_n = np.linspace(0, 1, 100)
+
+    plt.figure()
+    plt.subplot(211)
+    ax = plt.gca()
+    ax.plot(psi_n, eq.pressure(psi_N=psi_n), 'C1')
+    ax.set_xlabel(r'$\psi_\mathrm{N}$')
+    ax.set_ylabel(r'$p [Pa]$', color='C1')
+
+    ax2 = ax.twinx()
+    ax2.plot(psi_n, eq.pprime(psi_N=psi_n), color='C2')
+    ax2.set_ylabel(r"$p'$", color='C2')
+
+    plt.subplot(212)
+    ax = plt.gca()
+    ax.plot(psi_n, eq.fpol(psi_N=psi_n), 'C1')
+    ax.set_xlabel(r'$\psi_\mathrm{N}$')
+    ax.set_ylabel(r'$f$ ', color='C1')
+
+    ax2 = ax.twinx()
+    ax2.plot(psi_n, eq.ffprime(psi_N=psi_n), 'C2')
+    ax2.set_ylabel(r"$ff'$ ", color='C2')
 
 
 def main():
     import numpy as np
+    import matplotlib.pyplot as plt
 
     r = np.linspace(0.5, 2.5, 100)
     z = np.linspace(-1.5, 1.5, 200)
@@ -112,7 +199,8 @@ def main():
     #                         'Z': z})
 
     # eq_ds = load_gfile('/compass/home/kripner/COMPU/fiesta/natural_divertor_v666.gfile')
-    eq_ds = load_gfile('/compass/Shared/Exchange/imrisek/MATLAB/COMPASS_U/Scenarios/scenario_1_baseline_eqdsk')
+    gfile = '/compass/Shared/Exchange/imrisek/MATLAB/COMPASS_U/Scenarios/scenario_1_baseline_eqdsk'
+    eq_ds = load_gfile(gfile)
 
     # plt.pcolormesh(r_grid, z_grid, psi)
     # plt.gca().set_aspect('equal')
@@ -121,12 +209,19 @@ def main():
 
     print(eq_ds)
 
-    eq = Equilibrium(eq_ds)
+    # eq = Equilibrium(eq_ds)
+
+    eq = test_read_gfile(gfile)
 
     plot_over_view_2d(eq)
 
+    test_qprofiles(gfile, eq)
 
-#    plt.show()
+    print(eq.fluxfuncs.fpol)
+
+    print(eq.fluxfuncs.__dict__)
+
+    plt.show()
 
 
 
