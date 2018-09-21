ef6f02c6f4dbf044c5c6fe606989bcee4d98fa94
Lukas Kripner <kripner@ipp.cas.cz>
2018-09-21 11:53:29 +0200

    the spline creation function is now taking `s` and `k` as an input. Input is ordered and unique in psi_n.

--- pleque/core.py
+++ pleque/core.py
@@ -20,13 +20,21 @@ class FluxFuncs:
         for fn in _coordinates_funcs:
             setattr(self, fn, getattr(self._equi, fn))  # methods are bound to _equi
 
-
-    def add_flux_func(self, name, data, *coordinates, R=None, Z=None, psi_n=None, coord_type=None, **coords):
+    def add_flux_func(self, name, data, *coordinates, R=None, Z=None, psi_n=None, coord_type=None, spline_smooth=0,
+                      spline_order=3, **coords):
         from scipy.interpolate import UnivariateSpline
 
         coord = self.coordinates(*coordinates, R=R, Z=Z, psi_n=psi_n, coord_type=coord_type, **coords)
         # interp = interpolate(psi_n, data)
-        interp = UnivariateSpline(coord.psi_n, data, s=0, k=3)
+        # todo: unique
+        # psi_n = coord.psi_n
+        # idxs = np.argsort(psi_n)
+        # psi_n = psi_n[idxs]
+        # data = data[idxs]
+        psi_n, idxs = np.unique(coord.psi_n, return_index=True)
+        data = data[idxs]
+
+        interp = UnivariateSpline(psi_n, data, s=spline_smooth, k=spline_order)
         setattr(self, '_interp_' + name, interp)
 
         def new_func(self, *coordinates, R=None, Z=None, psi_n=None, coord_type=None, **coords):
