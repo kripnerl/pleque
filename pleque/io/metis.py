5a9f579c4ee75961690c6d112b1ad884f5d8477a
MatejTomes <tomes@ipp.cas.cz>
2018-10-07 17:00:44 +0200

    Metis omas
    Adding loading of metis data and fixing troubles with rong dimension order

--- pleque/io/metis.py
+++ pleque/io/metis.py
@@ -52,3 +52,35 @@ def read(equilibrium,file, time):
         fluxfun.add_flux_func(name, toexp[name].values,crds)
 
     return fluxfun
+
+def to_omas(fluxfunc, coordinates, ods = None, time = np.array([0], ndmin=1)):
+    """
+
+    :param fluxfunc: FluxFunction with metis profiles
+    :param coordinates: 1D coordinates specifying the independent variable (allows control over resolution).
+    :param ods: omas ODS file
+    :param time: ods data strucutr time to be used
+    :return: omas ods data structure
+    """
+    # TODO: So far only profiles to be used by ASCOT are saved. Others will be added later (or upon request)
+    import omas
+
+    if ods is None:
+        ods = omas.ODS()
+
+    ods["core_profiles"]["ids_properties"]["homogeneous_time"] = 1  #this has to be there for ["core_profiles"]["time"]
+    ods["core_profiles"]["time"] = np.array(time, ndmin=1)
+
+    #rho toroidal profiles
+    ods["core_profiles"]["profiles_1d"][0]["grid"]["rho_tor"] = fluxfunc._equi.tor_flux(coordinates)
+    ods["core_profiles"]["profiles_1d"][0]["grid"]["rho_tor_norm"] = (fluxfunc._equi.tor_flux(coordinates) /
+                                                                      fluxfunc._equi.tor_flux(psi_n=1))
+
+    #plasma profiles
+    ods["core_profiles"]["profiles_1d"][0]["ion"][0]["density"] = fluxfunc.nip(coordinates)
+    ods["core_profiles"]["profiles_1d"][0]["ion"][0]["temperature"] = fluxfunc.tip(coordinates)
+    ods["core_profiles"]["profiles_1d"][0]["ion"][0]["velocity"]["toroidal"] = fluxfunc.vtor(coordinates)
+    ods["core_profiles"]["profiles_1d"][0]["electrons"]["density"] = fluxfunc.nep(coordinates)
+    ods["core_profiles"]["profiles_1d"][0]["electrons"]["temperature"] = fluxfunc.tep(coordinates)
+
+    return ods
