7365153e1e542f183795eacb7e6069a23818376a
Lukas Kripner <kripner@ipp.cas.cz>
2018-10-09 15:26:07 +0200

    Cherry picked correction of OMAS
    save b0 as field and r0 (vacuum data)

--- pleque/io/omas.py
+++ pleque/io/omas.py
@@ -47,8 +47,8 @@ def write(equilibrium: Equilibrium, grid_1d = None, grid_2d=None, gridtype=1, od
 
     #vacuum
     #todo: add vacuum Btor, not in equilibrium
-    #ods["equilibrium"]["vacuum_toroidal_field"]["b0"] = np.array(B0_tor, ndmin=1) # vacuum B tor at Rmaj
-    #ods["equilibrium"]["vacuum_toroidal_field"]["r0"] = np.array(rmaj, ndmin=1) # vacuum B tor at Rmaj
+    ods["equilibrium"]["vacuum_toroidal_field"]["b0"] = np.array([5]) # vacuum B tor at Rmaj
+    ods["equilibrium"]["vacuum_toroidal_field"]["r0"] = 0.89 # vacuum B tor at Rmaj
 
     #time slice time
     ods["equilibrium"]["time_slice"][0]["time"] = time
@@ -88,7 +88,9 @@ def write(equilibrium: Equilibrium, grid_1d = None, grid_2d=None, gridtype=1, od
             from pleque.fluxsurface import FluxSurface
             surface = [FluxSurface(equilibrium.lcfs)]
 
-        if len(surface) > 0:
+        # todo: really 0 if open?
+        # todo: fix this 'surface[0].closed' ... maybe calculate it from boundary
+        if len(surface) > 0 and surface[0].closed:
             surface_volume[i] = surface[0].volume
             surface_area[i] = surface[0].area
         else:
