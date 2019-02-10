5647a9c59dda882dd1bf525fc3f5dff0aa027a2a
Lukas Kripner <kripner@ipp.cas.cz>
2019-02-10 23:04:39 +0100

    Prepare functions to convert xarrays to dictionaries.
    missing: conversion from dict to xarrays

--- pleque/io/tools.py
+++ pleque/io/tools.py
@@ -1,13 +1,14 @@
 import xarray as xr
 from collections import OrderedDict
+from typing import Union
 
 
-def xr2dict(ds: xr.Dataset):
+def xr2dict(ds: Union[xr.Dataset, xr.DataArray]):
     """
-    Convert all Dataset coordinates data and coordinates variables and all Dataset attributes into single dictionary.
+    Convert Dataset or DataArray to single dictionary.
 
-    :param ds:
-    :return: (OrderedDict) Dataset converted into single OrderedDict.
+    :param ds: [xr.Dataset, xr.DataArray]
+    :return: (dict) Dataset or DataArray converted into single OrderedDict.
     """
 
     ret_dict = OrderedDict()
@@ -15,3 +16,58 @@ def xr2dict(ds: xr.Dataset):
     ret_dict.update(ds.attrs)
 
     return ret_dict
+
+
+def da2dict(da: xr.DataArray):
+    """
+    Return representation of xarray DataArray as dictionary.
+    If DataArray has no name, data are returned under key "data".
+
+    :param da: DataArray
+    :return: dictionary with data from `da`
+    """
+
+    ret_dict = dict()
+    ret_dict.update(da.attrs)
+
+    # add data:
+    if da.name is None:
+        ret_dict["data"] = da.data
+    else:
+        ret_dict[da.name] = da.data
+
+    # Add axes:
+    for k, val in da.coords.items():
+        ret_dict[k] = val.data
+        # axis attributes:
+
+        for ka, atr in val.attrs.items():
+            ret_dict["{}/{}".format(k, ka)] = atr
+
+    return ret_dict
+
+
+def ds2dict(ds: xr.Dataset):
+    """
+    Convert all Dataset coordinates data and coordinates variables and all Dataset attributes into single dictionary.
+
+    :param ds:
+    :return: (dict) Dataset converted into single OrderedDict.
+    """
+
+    ret_dict = dict()
+    ret_dict.update(ds.attrs)
+
+    # Add all variables:
+    for k, val in ds.variables.items():
+        ret_dict[k] = val.data
+        for ka, atr in val.attrs.items():
+            ret_dict["{}/{}".format(k, ka)] = atr
+
+    return ret_dict
+
+
+
+
+def dict2xr(dictionary: dict):
+    pass
