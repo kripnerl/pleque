14e2a54a5b93301430d75307bba3f2636770f1ea
kripner <kripner@ipp.cas.cz>
2019-07-06 18:24:28 +0200

    fix: coordinates always return 1d array

--- pleque/core/coordinates.py
+++ pleque/core/coordinates.py
@@ -282,7 +282,7 @@ class Coordinates(object):
         coor1 = geometry.linestring.LineString(self.as_array(dim=dim_))
         coor2 = geometry.linestring.LineString(coords2.as_array(dim=dim_))
         intersec = coor1.intersection(coor2)
-        if isinstance(intersec, geometry.MultiLineString):
+        if isinstance(intersec, geometry.MultiLineString) or intersec.is_empty:
             return None
         elif intersec is not None:
             intersec = np.array(intersec).T
@@ -298,13 +298,13 @@ class Coordinates(object):
         :param coord_type: not effected at the moment (TODO)
         :return:
         """
-        # TODO integrate with numpy _as_array 
+        # TODO integrate with numpy _as_array
 
         if self.dim == 0:
             return np.array(())
         # coord_type_ = self._verify_coord_type(coord_type)
         elif dim == 1 or self.dim == 1:
-            return self.x1
+            return np.asanyarray(self.x1)
         elif dim == 2 or self.dim == 2:
             if self.grid:
                 x1, x2 = self.mesh()
@@ -313,22 +313,51 @@ class Coordinates(object):
                 # x2 = x2.ravel()
                 # return np.array([x1, x2]).T
             else:
-                return np.array([self.x1, self.x2]).T
+                return np.atleast_2d([self.x1, self.x2]).T
         elif dim == 3 or self.dim == 3:
             # todo: replace this by split method
-            return np.array([self.x1, self.x2, self.x3]).T
+            return np.asarray([self.x1, self.x2, self.x3]).T
 
     @property
-    def dl(self):
-        if not hasattr(self, '_dl'):
-            self._dl = np.sqrt((self.R[1:] - self.R[:-1]) ** 2 + (self.Z[1:] - self.Z[:-1]) ** 2)
-        return self._dl
+    def dists(self):
+        """
+        distances between spatial steps along the tracked field line
+        :return:
+        self._dists
+        """
+        if self.grid:
+            raise TypeError('The grid is used - no distances between spatial steps will be calculated')
+        if not hasattr(self, '_dists'):
+            if self.dim == 1:
+                self._dists = (self.x1[1:] - self.x1[:-1])
+            elif self.dim == 2:
+                self._dists = np.sqrt((self.x1[1:] - self.x1[:-1]) ** 2 + (self.x2[1:] - self.x2[:-1]) ** 2)
+            elif self.dim == 3:
+                self._dists = np.sqrt((self.x1[1:] - self.x1[:-1]) ** 2 + (self.x2[1:] - self.x2[:-1]) ** 2 +
+                                      (self.x3[1:] - self.x3[:-1]) ** 2)
+        return self._dists
 
     @property
     def cum_length(self):
+        """
+        Cumulative length along the coordinate points.
+
+        :return: array(N)
+        """
         if not hasattr(self, '_cum_length'):
-            self._cum_length = np.hstack((0, np.cumsum(self.dl)))
+            self._cum_length = np.hstack((0, np.cumsum(self.dists)))
         return self._cum_length
+
+    @property
+    def length(self):
+        """
+        Total length along the coordinate points.
+
+        :return: length in meters
+        """
+        if not hasattr(self, '_cum_length'):
+            self._cum_length = np.hstack((0, np.cumsum(self.dists)))
+        return self._cum_length[-1]
 
     def _evaluate_input(self, *coordinates, coord_type=None, **coords):
         from collections import Iterable
@@ -445,9 +474,9 @@ class Coordinates(object):
                 self._x2_input = x2
             elif len(coordinates) == 3:
                 self.dim = 3
-                x1 = coordinates[0]
-                x2 = coordinates[1]
-                x3 = coordinates[2]
+                x1 = np.atleast_1d(coordinates[0])
+                x2 = np.atleast_1d(coordinates[1])
+                x3 = np.atleast_1d(coordinates[2])
 
                 # assume _x1_input and _x2_input to be arrays of size (N)
                 if not isinstance(x1, np.ndarray):
@@ -540,6 +569,8 @@ class Coordinates(object):
                 self.x1 = self._x1_input ** 2
             else:
                 raise ValueError('This should not happen.')
+            self.x1 = np.array(self.x1, copy=False, ndmin=1)
+
         elif self.dim == 2:
             # only (R, Z) coordinates are implemented now
             if self._coord_type_input == ('R', 'Z'):
@@ -551,13 +582,16 @@ class Coordinates(object):
                 cc = - self.cocos_dict['sigma_pol'] * self.cocos_dict['sigma_cyl']
                 self.x1 = r_mgax + self._x1_input * np.cos(self._x2_input)
                 self.x2 = z_mgax + cc * self._x1_input * np.sin(self._x2_input)
+            self.x1 = np.array(self.x1, copy=False, ndmin=1)
+            self.x2 = np.array(self.x2, copy=False, ndmin=1)
+
         elif self.dim == 3:
             # only (R, Z) coordinates are implemented now
             # if self._coord_type_input == ('R', 'Z', 'phi'):
             if any([p == ('R', 'Z', 'phi') for p in itertools.permutations(self._coord_type_input)]):
-                self.x1 = self._x1_input
-                self.x2 = self._x2_input
-                self.x3 = self._x3_input
+                self.x1 = np.asanyarray(self._x1_input)
+                self.x2 = np.asanyarray(self._x2_input)
+                self.x3 = np.asanyarray(self._x3_input)
             # elif self._coord_type_input == ('X', 'Y', 'Z'):
             elif any([p == ('X', 'Y', 'Z') for p in itertools.permutations(self._coord_type_input)]):
                 # todo: COCOS
@@ -568,3 +602,7 @@ class Coordinates(object):
                 self.x1 = np.sqrt(self._x1_input ** 2 + self._x2_input ** 2)
                 self.x2 = self._x3_input
                 self.x3 = np.arctan2(cc * self._x2_input, self._x1_input)
+
+            self.x1 = np.array(self.x1, copy=False, ndmin=1)
+            self.x2 = np.array(self.x2, copy=False, ndmin=1)
+            self.x3 = np.array(self.x3, copy=False, ndmin=1)
