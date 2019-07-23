fix: o-points, x-points testing (higher precision)

pleque/core/equilibrium.py
@@ -190,6 +190,7 @@ class Equilibrium(object):
         self._mg_axis, sortidx = eq_tools.recognize_mg_axis(o_points, self._spl_psi, r_lim, z_lim, self._mg_axis)
         self._psi_axis = np.asscalar(self._spl_psi(self._mg_axis[0], self._mg_axis[1], grid=False))
         self._o_points = o_points[sortidx]
+        self._o_points[0] = self._mg_axis
 
         # ------------------------------------------
         # Recognize x-point plasma vs limiter plasma
@@ -210,6 +211,10 @@ class Equilibrium(object):
             self._psi_xp = self._spl_psi(*xp1, grid=False)
 
         self._x_points = x_points[sortidx]
+        if xp1 is not None:
+            self._x_points[0] = xp1
+        if xp2 is not None:
+            self._x_points[1] = xp2
 
         limiter_plasma, limiter_point = eq_tools.recognize_plasma_type(self._x_point, self._first_wall,
                                                                        self._mg_axis, self._psi_axis, self._spl_psi)
@@ -1061,9 +1066,11 @@ class Equilibrium(object):
             else:
                 phi0 = coords.phi[i]
 
-            xp = self._x_point
-            xp_dist = np.sqrt(np.sum((xp - y0) ** 2))
-            atol = np.minimum(xp_dist * 1e-3, 1e-6)
+            atol = 1e-6
+            if self.is_xpoint_plasma:
+                xp = self._x_point
+                xp_dist = np.sqrt(np.sum((xp - y0) ** 2))
+                atol = np.minimum(xp_dist * 1e-3, atol)
 
             if self._verbose:
                 print('>>> tracing from: {:3f},{:3f},{:3f}'.format(y0[0], y0[1], phi0))

pleque/utils/equi_tools.py
@@ -30,7 +30,7 @@ def minimize_in_vicinity(point, func, r_lims, z_lims):
               (np.max((z_lims[0], point[1] - 0.1)),
                np.min((z_lims[-1], point[1] + 0.1))))
 
-    res = minimize(func, point, method='Powell')
+    res = minimize(func, point, method='Powell', options={'xtol': 1e-7})
     # res = minimize(func, point, bounds=bounds)
     res_point = np.array((res['x'][0], res['x'][1]))
     return res_point

tests/test_cocos.py
@@ -123,7 +123,8 @@ def test_directions(geqdsk_file, cocos):
     assert my_dir == dphidtheta
 
 
-def test_coordinates_transforms(geqdsk_file):
+@pytest.mark.parametrize(('cocos',), [[1], [2], [3], [4], [5], [6], [7], [8]])
+def test_coordinates_transforms(geqdsk_file, cocos):
     """
     Octants: https://en.wikipedia.org/wiki/Octant_(solid_geometry)#/media/File:Octant_numbers.svg
 
@@ -131,137 +132,135 @@ def test_coordinates_transforms(geqdsk_file):
     :return:
     """
 
-    for cocos in range(1, 9):
-        print('COCOS: {}'.format(cocos))
+    eq = read_geqdsk(geqdsk_file, cocos=cocos)
+    mgax = eq.magnetic_axis
 
-        eq = read_geqdsk(geqdsk_file, cocos=cocos)
-        mgax = eq.magnetic_axis
+    # 3d octants:
+    o1 = eq.coordinates(X=1, Y=1, Z=1)
+    o2 = eq.coordinates(X=-1, Y=1, Z=1)
+    o3 = eq.coordinates(X=-1, Y=-1, Z=1)
+    o4 = eq.coordinates(X=1, Y=-1, Z=1)
 
-        # 3d octants:
-        o1 = eq.coordinates(X=1, Y=1, Z=1)
-        o2 = eq.coordinates(X=-1, Y=1, Z=1)
-        o3 = eq.coordinates(X=-1, Y=-1, Z=1)
-        o4 = eq.coordinates(X=1, Y=-1, Z=1)
+    o5 = eq.coordinates(X=1, Y=1, Z=-1)
+    o6 = eq.coordinates(X=-1, Y=1, Z=-1)
+    o7 = eq.coordinates(X=-1, Y=-1, Z=-1)
+    o8 = eq.coordinates(X=1, Y=-1, Z=-1)
 
-        o5 = eq.coordinates(X=1, Y=1, Z=-1)
-        o6 = eq.coordinates(X=-1, Y=1, Z=-1)
-        o7 = eq.coordinates(X=-1, Y=-1, Z=-1)
-        o8 = eq.coordinates(X=1, Y=-1, Z=-1)
+    # 2d quadrants:
+    q1 = eq.coordinates(R=mgax.R + 0.5, Z=mgax.Z + 0.5)
+    q2 = eq.coordinates(R=mgax.R - 0.5, Z=mgax.Z + 0.5)
+    q3 = eq.coordinates(R=mgax.R - 0.5, Z=mgax.Z - 0.5)
+    q4 = eq.coordinates(R=mgax.R + 0.5, Z=mgax.Z - 0.5)
 
-        # 2d quadrants:
-        q1 = eq.coordinates(R=mgax.R + 0.5, Z=mgax.Z + 0.5)
-        q2 = eq.coordinates(R=mgax.R - 0.5, Z=mgax.Z + 0.5)
-        q3 = eq.coordinates(R=mgax.R - 0.5, Z=mgax.Z - 0.5)
-        q4 = eq.coordinates(R=mgax.R + 0.5, Z=mgax.Z - 0.5)
+    # toroidal angle:
+    if cocos in [1, 3, 5, 7]:
+        # Cnt-clockwise
+        assert 0 < np.mod(o1.phi, 2 * np.pi) < np.pi / 2
+        assert np.pi / 2 < np.mod(o2.phi, 2 * np.pi) < np.pi
+        assert np.pi < np.mod(o3.phi, 2 * np.pi) < 3 * np.pi / 2
+        assert 3 * np.pi / 2 < np.mod(o4.phi, 2 * np.pi) < 2 * np.pi
 
-        # toroidal angle:
-        if cocos in [1, 3, 5, 7]:
-            # Cnt-clockwise
-            assert 0 < np.mod(o1.phi, 2 * np.pi) < np.pi / 2
-            assert np.pi / 2 < np.mod(o2.phi, 2 * np.pi) < np.pi
-            assert np.pi < np.mod(o3.phi, 2 * np.pi) < 3 * np.pi / 2
-            assert 3 * np.pi / 2 < np.mod(o4.phi, 2 * np.pi) < 2 * np.pi
+        assert 0 < np.mod(o5.phi, 2 * np.pi) < np.pi / 2
+        assert np.pi / 2 < np.mod(o6.phi, 2 * np.pi) < np.pi
+        assert np.pi < np.mod(o7.phi, 2 * np.pi) < 3 * np.pi / 2
+        assert 3 * np.pi / 2 < np.mod(o8.phi, 2 * np.pi) < 2 * np.pi
 
-            assert 0 < np.mod(o5.phi, 2 * np.pi) < np.pi / 2
-            assert np.pi / 2 < np.mod(o6.phi, 2 * np.pi) < np.pi
-            assert np.pi < np.mod(o7.phi, 2 * np.pi) < 3 * np.pi / 2
-            assert 3 * np.pi / 2 < np.mod(o8.phi, 2 * np.pi) < 2 * np.pi
+    else:
+        # Clockwise
+        assert 0 < np.mod(o4.phi, 2 * np.pi) < np.pi / 2
+        assert np.pi / 2 < np.mod(o3.phi, 2 * np.pi) < np.pi
+        assert np.pi < np.mod(o2.phi, 2 * np.pi) < 3 * np.pi / 2
+        assert 3 * np.pi / 2 < np.mod(o1.phi, 2 * np.pi) < 2 * np.pi
 
-        else:
-            # Clockwise
-            assert 0 < np.mod(o4.phi, 2 * np.pi) < np.pi / 2
-            assert np.pi / 2 < np.mod(o3.phi, 2 * np.pi) < np.pi
-            assert np.pi < np.mod(o2.phi, 2 * np.pi) < 3 * np.pi / 2
-            assert 3 * np.pi / 2 < np.mod(o1.phi, 2 * np.pi) < 2 * np.pi
+        assert 0 < np.mod(o8.phi, 2 * np.pi) < np.pi / 2
+        assert np.pi / 2 < np.mod(o7.phi, 2 * np.pi) < np.pi
+        assert np.pi < np.mod(o6.phi, 2 * np.pi) < 3 * np.pi / 2
+        assert 3 * np.pi / 2 < np.mod(o5.phi, 2 * np.pi) < 2 * np.pi
 
-            assert 0 < np.mod(o8.phi, 2 * np.pi) < np.pi / 2
-            assert np.pi / 2 < np.mod(o7.phi, 2 * np.pi) < np.pi
-            assert np.pi < np.mod(o6.phi, 2 * np.pi) < 3 * np.pi / 2
-            assert 3 * np.pi / 2 < np.mod(o5.phi, 2 * np.pi) < 2 * np.pi
+    # Poloidal angle:
+    if cocos in [2, 3, 5, 8]:
+        # Cnt-clockwise
+        assert 0 * np.pi < np.mod(q1.theta[0], 2 * np.pi) < np.pi / 2
+        assert np.pi / 2 < np.mod(q2.theta[0], 2 * np.pi) < np.pi
+        assert np.pi < np.mod(q3.theta[0], 2 * np.pi) < 3 * np.pi / 2
+        assert 3 * np.pi / 2 < np.mod(q4.theta[0], 2 * np.pi) < 2 * np.pi
+    else:
+        assert 0 * np.pi < np.mod(q4.theta[0], 2 * np.pi) < np.pi / 2
+        assert np.pi / 2 < np.mod(q3.theta[0], 2 * np.pi) < np.pi
+        assert np.pi < np.mod(q2.theta[0], 2 * np.pi) < 3 * np.pi / 2
+        assert 3 * np.pi / 2 < np.mod(q1.theta[0], 2 * np.pi) < 2 * np.pi
 
-        # Poloidal angle:
-        if cocos in [2, 3, 5, 8]:
-            # Cnt-clockwise
-            assert 0 * np.pi < np.mod(q1.theta[0], 2 * np.pi) < np.pi / 2
-            assert np.pi / 2 < np.mod(q2.theta[0], 2 * np.pi) < np.pi
-            assert np.pi < np.mod(q3.theta[0], 2 * np.pi) < 3 * np.pi / 2
-            assert 3 * np.pi / 2 < np.mod(q4.theta[0], 2 * np.pi) < 2 * np.pi
-        else:
-            assert 0 * np.pi < np.mod(q4.theta[0], 2 * np.pi) < np.pi / 2
-            assert np.pi / 2 < np.mod(q3.theta[0], 2 * np.pi) < np.pi
-            assert np.pi < np.mod(q2.theta[0], 2 * np.pi) < 3 * np.pi / 2
-            assert 3 * np.pi / 2 < np.mod(q1.theta[0], 2 * np.pi) < 2 * np.pi
+    # Toroidal quadrants::
+    o1 = eq.coordinates(R=mgax.R, Z=mgax.Z, phi=np.pi / 4)
+    o2 = eq.coordinates(R=mgax.R, Z=mgax.Z, phi=3 * np.pi / 4)
+    o3 = eq.coordinates(R=mgax.R, Z=mgax.Z, phi=5 * np.pi / 4)
+    o4 = eq.coordinates(R=mgax.R, Z=mgax.Z, phi=7 * np.pi / 4)
 
-        # Toroidal quadrants::
-        o1 = eq.coordinates(R=mgax.R, Z=mgax.Z, phi=np.pi / 4)
-        o2 = eq.coordinates(R=mgax.R, Z=mgax.Z, phi=3 * np.pi / 4)
-        o3 = eq.coordinates(R=mgax.R, Z=mgax.Z, phi=5 * np.pi / 4)
-        o4 = eq.coordinates(R=mgax.R, Z=mgax.Z, phi=7 * np.pi / 4)
+    # Poloidal quadrants:
+    q1 = eq.coordinates(r=0.1, theta=1 * np.pi / 4)
+    q2 = eq.coordinates(r=0.1, theta=3 * np.pi / 4)
+    q3 = eq.coordinates(r=0.1, theta=5 * np.pi / 4)
+    q4 = eq.coordinates(r=0.1, theta=7 * np.pi / 4)
 
-        # Poloidal quadrants:
-        q1 = eq.coordinates(r=0.1, theta=1 * np.pi / 4)
-        q2 = eq.coordinates(r=0.1, theta=3 * np.pi / 4)
-        q3 = eq.coordinates(r=0.1, theta=5 * np.pi / 4)
-        q4 = eq.coordinates(r=0.1, theta=7 * np.pi / 4)
+    # toroidal angle:
+    if cocos in [1, 3, 5, 7]:
+        # Cnt-clockwise
+        assert 0 < o1.X
+        assert 0 < o1.Y
 
-        # toroidal angle:
-        if cocos in [1, 3, 5, 7]:
-            # Cnt-clockwise
-            assert 0 < o1.X
-            assert 0 < o1.Y
+        assert 0 > o2.X
+        assert 0 < o2.Y
 
-            assert 0 > o2.X
-            assert 0 < o2.Y
+        assert 0 > o3.X
+        assert 0 > o3.Y
 
-            assert 0 > o3.X
-            assert 0 > o3.Y
+        assert 0 < o4.X
+        assert 0 > o4.Y
+    else:
+        # Clockwise
+        assert 0 < o1.X
+        assert 0 > o1.Y
 
-            assert 0 < o4.X
-            assert 0 > o4.Y
-        else:
-            # Clockwise
-            assert 0 < o1.X
-            assert 0 > o1.Y
+        assert 0 > o2.X
+        assert 0 > o2.Y
 
-            assert 0 > o2.X
-            assert 0 > o2.Y
+        assert 0 > o3.X
+        assert 0 < o3.Y
 
-            assert 0 > o3.X
-            assert 0 < o3.Y
+        assert 0 < o4.X
+        assert 0 < o4.Y
 
-            assert 0 < o4.X
-            assert 0 < o4.Y
+    # Poloidal angle:
+    if cocos in [2, 3, 5, 8]:
+        # Cnt-clockwise
+        assert mgax.R < q1.R
+        assert mgax.Z < q1.Z
 
-        # Poloidal angle:
-        if cocos in [2, 3, 5, 8]:
-            # Cnt-clockwise
-            assert mgax.R < q1.R
-            assert mgax.Z < q1.Z
+        assert mgax.R > q2.R
+        assert mgax.Z < q2.Z
 
-            assert mgax.R > q2.R
-            assert mgax.Z < q2.Z
+        assert mgax.R > q3.R
+        assert mgax.Z > q3.Z
 
-            assert mgax.R > q3.R
-            assert mgax.Z > q3.Z
+        assert mgax.R < q4.R
+        assert mgax.Z > q4.Z
+    else:
+        # Clockwise
+        assert mgax.R < q1.R
+        assert mgax.Z > q1.Z
 
-            assert mgax.R < q4.R
-            assert mgax.Z > q4.Z
-        else:
-            # Clockwise
-            assert mgax.R < q1.R
-            assert mgax.Z > q1.Z
+        assert mgax.R > q2.R
+        assert mgax.Z > q2.Z
 
-            assert mgax.R > q2.R
-            assert mgax.Z > q2.Z
+        assert mgax.R > q3.R
+        assert mgax.Z < q3.Z
 
-            assert mgax.R > q3.R
-            assert mgax.Z < q3.Z
-
-            assert mgax.R < q4.R
-            assert mgax.Z < q4.Z
+        assert mgax.R < q4.R
+        assert mgax.Z < q4.Z
 
 
-def test_cocos_consistency(geqdsk_file):
+@pytest.mark.parametrize(('cocos',), [[1], [2], [3], [4], [5], [6], [7], [8]])
+def test_cocos_consistency(geqdsk_file, cocos):
     for cocos in range(1, 9):
         print('COCOS: {}'.format(cocos))
 