d222d9f1f935521cef688ab4e60e97512002da13
Ondrej Ficker <oficker@gmail.com>
2019-10-21 22:41:37 +0200

    modified interpolation using Coordinates class

--- examples/example_impact_angle.py
+++ examples/example_impact_angle.py
@@ -10,6 +10,7 @@ sys.path.append('C:\\Users\\Ondrej\\Projects\\pleque\\')
 import matplotlib.pyplot as plt
 
 from pleque.tests.utils import load_testing_equilibrium
+from pleque.core.coordinates import Coordinates
 import numpy as np
 from scipy.interpolate import splprep, splev
 
@@ -27,25 +28,28 @@ fign,axn=plt.subplots()
 axn.plot(first_wall.R,first_wall.Z)
 
 
-point = eq.coordinates(R=0.83, Z=-0.3)
-surf_frompoint = eq._flux_surface(point)
-surf_frompoint = eq._flux_surface(point)
-
 grid = eq.grid(resolution=[1e-3, 2e-3], dim="step")
 
+coords=Coordinates(eq,np.vstack((first_wall.R,first_wall.Z)).T)
 
-def interpolate_lim(first_wall,npoints):
+
+def interpolate_lim(coords, npoints):
     """
-    Implicit spline curve interpolation for the limiter, number of points to be specified
-    should specify
+    Implicit spline curve interpolation for the limiter, number of points must be specified
+
     """
+    
     dR=-np.diff(first_wall.R)
     dZ=-np.diff(first_wall.Z)
     
-    dists=np.cumsum(np.sqrt(dR**2+dZ**2))
+    
+    dists=coords.cum_length
+    
+    #dists=np.cumsum(np.sqrt(dR**2+dZ**2))
     #print(np.shape(first_wall.R[:-1]),np.shape(dists))
     #print(dists)
-    tck, u = splprep([first_wall.R[:-1], first_wall.Z[:-1]],u=dists,k=1,s=0)
+    
+    tck, u = splprep([coords.R, first_wall.Z],u=dists,k=1,s=0)
     t=np.linspace(np.amin(u),np.amax(u),npoints)
     newpoints = splev(t, tck)
     return newpoints[0],newpoints[1]
@@ -140,7 +144,7 @@ def impact_angle(eq, first_wall):
     
     bvec=bvec_at_limiter(eq,first_wall)
     #print(np.shape(Bvec[:,:-1]),np.shape(normal_vecs))
-    impcos=np.einsum('ij,ij->j', bvec[:,:-1], normal_vecs.T,)
+    impcos=np.einsum('ij,ij->j', bvec[:,:-1], normal_vecs.T)
     
     return np.arccos(impcos)
     
@@ -158,7 +162,7 @@ vec=np.linspace(0,0.1,2)
 
 impang=impact_angle(eq,newpoints)
 
-
+axn.set_aspect('equal')
     
 for i in range(0,npoints-1):
     axn.plot(newpoints[0][i]+normal_vecs[0][i]*vec,newpoints[1][i]+normal_vecs[1][i]*vec,color='k')
@@ -172,6 +176,11 @@ print(np.shape(impang))
 
 fig,ax=plt.subplots()
 
+grid = eq.grid(resolution=[1e-3, 2e-3], dim="step")
+# psipol_map = Coordinates(eq, R = r, Z=z, grid=True)
+cl = axn.contour(grid.R, grid.Z, grid.psi_n, 200)
+
+ax.set_aspect('equal')
 cmap=plt.get_cmap('jet')
 for i in range(0,npoints-1):
     ax.scatter(newpoints[0][i], newpoints[1][i], c = cmap((impang[i]-np.amin(impang))/(np.amax(impang)-np.amin(impang))))
