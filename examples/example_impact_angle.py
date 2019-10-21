783f23d661e0e0c6d963029d46fd23f9bca593ed
Ondrej Ficker <oficker@gmail.com>
2019-10-21 18:04:57 +0200

    improved impact angle example - doctrings, checking plots, shorter functions

--- examples/example_impact_angle.py
+++ examples/example_impact_angle.py
@@ -11,25 +11,16 @@ import matplotlib.pyplot as plt
 
 from pleque.tests.utils import load_testing_equilibrium
 import numpy as np
-
-
-import matplotlib.pyplot as plt
-
-
-from pleque.io import _geqdsk as eqdsktool 
-from pleque.io.readers import read_geqdsk
-from pleque.utils.plotting import *
-from pleque import Equilibrium
-from pleque.tests.utils import get_test_equilibria_filenames, load_testing_equilibrium
 from scipy.interpolate import splprep, splev
-#gfiles = get_test_equilibria_filenames()
-#eq = read_geqdsk(gfiles[6])
+
 
 eq = load_testing_equilibrium(4)
 
 surf_lcfs = eq._flux_surface(psi_n=1 - 1e-6)[0]
-lim=eq.first_wall
+first_wall=eq.first_wall
 print(dir(lim))
+
+npoints=500
 
 plt.plot(lim.R,lim.Z)
 plt.show()
@@ -39,83 +30,147 @@ surf_frompoint = eq._flux_surface(point)
 surf_frompoint = eq._flux_surface(point)
 
 grid = eq.grid(resolution=[1e-3, 2e-3], dim="step")
-#grint(grid.R)
 
-#Br=eq.B_R(point)
-#Bz=eq.B_Z(point)
-#Btor=eq.B_tor(point)
 
-#print(Br,Bz,Btor)
-
-def interpolate_lim(lim):
-    tck, u = splprep([lim.R, lim.Z], s=0)
-    t=np.linspace(np.amin(u),np.amax(u),500)
+def interpolate_lim(first_wall,npoints):
+    """
+    Implicit spline curve interpolation fir the limiter, number of points, should specify
+    """
+    dR=-np.diff(first_wall.R)
+    dZ=-np.diff(first_wall.Z)
+    
+    dists=np.cumsum(np.sqrt(dR**2+dZ**2))
+    print(np.shape(lim.R[:-1]),np.shape(dists))
+    print(dists)
+    tck, u = splprep([lim.R[:-1], lim.Z[:-1]],u=dists,k=1,s=0)
+    t=np.linspace(np.amin(u),np.amax(u),npoints)
     newpoints = splev(t, tck)
     return newpoints[0],newpoints[1]
 
-newpoints=interpolate_lim(lim)
+newpoints=interpolate_lim(first_wall,npoints)
 
-def lim_norm_vec_splined(lim):
-    dR=-np.diff(lim[0])
-    dZ=-np.diff(lim[1])
+def limiter_norm_vec_splined(first_wall):
+    """Calculate limiter fw input as list of R and Z cooridnate arrays
+    
+    """
+    dR=-np.diff(first_wall[0])
+    dZ=-np.diff(first_wall[1])
     lim_vec=np.vstack((dR,dZ,np.zeros(np.shape(dR))))
     print(np.shape(lim_vec))
     pol=lim_vec/np.linalg.norm(lim_vec,axis=0)
 
     tor=[0,0,1]
-    return np.cross(pol,tor,axis=0)
+    
+    normal=np.cross(pol,tor,axis=0)/np.linalg.norm(np.cross(pol,tor,axis=0))
+    
+    return normal
 
-def lim_norm_vec(lim):
-    dR=-np.diff(lim.R)
-    dZ=-np.diff(lim.Z)
+def limimiter_norm_vec(first_wall):
+    """Calculate limiter nomrmal vector with fw input directly from eq class
+    
+    """
+    dR=-np.diff(first_wall.R)
+    dZ=-np.diff(first_wall.Z)
     lim_vec=np.vstack((dR,dZ,np.zeros(np.shape(dR))))
     print(np.shape(lim_vec))
     pol=lim_vec/np.linalg.norm(lim_vec,axis=0)
 
     tor=[0,0,1]
-    return np.cross(pol,tor,axis=0)
+    
+    normal=np.cross(pol,tor,axis=0)/np.linalg.norm(np.cross(pol,tor,axis=0))
+    
+    return normal
+    
+def bvec_at_limiter(eq, first_wall):
+    """Impact angle calculation - dot product of PFC norm and local magnetic field direction
+    
+    Parameters
+    ----------
+    eq: object equilibrium
+    firstwall: interpolated first wall
+        
+    Returns
+    -------
+    Bvec: magnetic field direction at the limiter
+        
+    Note
+    ----
     
     
-
-def impact_angle(eq, first_wall):
     """
-    Attempt to create firs iteration of an impact angle.
-    
-    """
-    
-    normal_vecs=lim_norm_vec_splined(first_wall).T
     fwr=first_wall[0]
     fwz=first_wall[1]
     fwphi=np.zeros(np.shape(fwr))
+    
     first_wall_transp=np.vstack([fwr,fwz,fwphi]).T
-    BR=eq.B_R(first_wall_transp)
     
-    #ax.plot(fwz,BR)
-    Bz=eq.B_Z(first_wall_transp)
-    Btor=eq.B_tor(first_wall_transp)
     
-    Bvec=np.vstack((BR,Bz,Btor))
-    Bvec=Bvec/np.linalg.norm(Bvec,axis=0)
-    print(np.shape(Bvec[:,:-1]),np.shape(normal_vecs))
-    #impang=np.dot(Bvec[:,:-1],normal_vecs.T,axes=0)
-    impcos=np.einsum('ij,ij->j', Bvec[:,:-1], normal_vecs.T,)
+    bR=eq.B_R(first_wall_transp)
+    bz=eq.B_Z(first_wall_transp)
+    btor=eq.B_tor(first_wall_transp)
+    
+    bvec=np.vstack((bR,bz,btor))
+    bvec=bvec/np.linalg.norm(bvec,axis=0)
+    
+    return bvec
+   
+
+def impact_angle(eq, first_wall):
+    """Impact angle calculation - dot product of PFC norm and local magnetic field direction
+    
+    Parameters
+    ----------
+    eq: object equilibrium
+    firstwall: interpolated first wall
+        
+    Returns
+    -------
+    impang: impact angle in rad
+        
+    Note
+    ----
+    
+    
+    """
+    
+    normal_vecs=limiter_norm_vec_splined(first_wall).T
+    
+    bvec=bvec_at_limiter(eq,first_wall)
+    #print(np.shape(Bvec[:,:-1]),np.shape(normal_vecs))
+    impcos=np.einsum('ij,ij->j', bvec[:,:-1], normal_vecs.T,)
+    
     return np.arccos(impcos)
     
-normal_vecs=lim_norm_vec_splined(newpoints)
 
-#print(np.shape(normal_vecs))
 
-#plt.plot(normal_vecs[1,:])
-#length=0.02
-vec=np.linspace(0,0.01,2)
+# test area
+    
+
+normal_vecs=limiter_norm_vec_splined(newpoints)
+
+bvec=bvec_at_limiter(eq, newpoints)
+
+vec=np.linspace(0,0.1,2)
 
 impang=impact_angle(eq,newpoints)
 
-for i in range(0,499):
-    plt.plot(newpoints[0][i]+normal_vecs[0][i]*vec,newpoints[1][i]+normal_vecs[1][i]*vec)
+fign,axn=plt.subplots()
+    
+for i in range(0,npoints-1):
+    axn.plot(newpoints[0][i]+normal_vecs[0][i]*vec,newpoints[1][i]+normal_vecs[1][i]*vec,color='k')
+    
+figb,axb=plt.subplots()
+    
+for i in range(0,npoints-1):
+    axn.plot(newpoints[0][i]+bvec[0][i]*vec,newpoints[1][i]+bvec[1][i]*vec,color='r')
     
 print(np.shape(impang))
 
 fig,ax=plt.subplots()
-ax.plot(impang)
+
+cmap=plt.get_cmap('jet')
+for i in range(0,npoints-1):
+    ax.scatter(newpoints[0][i], newpoints[1][i], c = cmap((impang[i]-np.amin(impang))/(np.amax(impang)-np.amin(impang))))
     
+fig2,ax2=plt.subplots()
+ax2.plot()
