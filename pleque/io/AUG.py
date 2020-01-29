8bbd08dd6bc86406bd35c6f72daa6ed0c0958290
Ondrej Grover <ondrej.grover@gmail.com>
2020-01-29 15:07:24 +0100

    WIP: working data loading
    had to use map_equ instead of the standard dd since the current dd has issues on PY3
    
    Could benefit form using extra hints

--- pleque/io/AUG.py
+++ pleque/io/AUG.py
@@ -3,11 +3,16 @@
 Supports the CLISTE outputs EQI and EQH
 """
 
-import numpy as np
+from scipy.constants import pi, mu_0
 import xarray as xr
 from .tools import EquilibriaTimeSlices
 
-import dd                       # the AUG data reading library
+# Python helper for working with AUG equilibria
+# NOTE uses a different dd library than the standard one
+import map_equ
+
+_2PI = 2*pi
+
 
 def read_aug_eq(shot_no, eq_type='EQH', time=None, experiment='AUGD', edition=0):
     """Read and AUG equilibria from a shotfile
@@ -21,44 +26,45 @@ def read_aug_eq(shot_no, eq_type='EQH', time=None, experiment='AUGD', edition=0)
 
     :return Equilibrium or EquilibriaTimeSlices: depending on time given
     """
-    shotfile = dd.shotfile(shot, eq_type, experiment, edition)
-    pres = shotfile('Pres')     # defined with respect to pfl
-    pfl = pres.area.data # poloidal flux label (psi)  TODO is the area safe?
-    time = pres.time.data
-    pressure = pres.data[::2]        # every second value is pprime
-    pprime = pres.data[1::2]
-    ffprime = shotfile('FFP').data  # TODO small F or f?
-    q = shotfile('Qpsi').data
-    psi_RZ = shotfile('PFM')  # Poloidal Flux Matrix; index1=i(0:M);index2=j(0:N); ind3= time
-    R = shotfile('Ri')
-    Z = shotfile('Zj')
-    # the *xx data have index: mag. axis, separatrix, limiter, 2nd sep., 2nd. limiter
-    pfxx = shotfile('PFxx')     # psi at interesting points
-    # TODO RPFx and zPFx might also be useful as starting points
-    psi_axis = pfxx.data[0]
-    psi_lcfs = pfxx.data[1]
-    # TODO should pleque be given psi and define psi_n after LCFS and axis are found for consistence?
-    psi_n_factor = (psi_lcfs - psi_axis)    # factor for psi_n derivatives
-    psi_n = (pfl - psi_axis) / psi_n_factor  # TODO should be a function
+    eq_aug = map_equ.equ_map(shot_no, eq_type, experiment, edition)
+    eq_aug.read_pfm()       # reads the poloidal flux matrix
+    eq_aug.read_scalars()   # such as psix=pci_lcfs
+    Ipol, dIpol = eq_aug.get_mixed('Jpol')  # poloidal current Ip(psi) and deriv
+    ffprime = eq_aug.get_profile('FFP')    # ff'   (f=F * mu_0)
+    F = Ipol / _2PI
+    Fprime = dIpol / _2PI
+    FFprime = ffprime * mu_0**2
+    pressure, pprime = eq_aug.get_mixed('Pres')
+    q = eq_aug.get_profile('Qpsi')  # q(psi)
+    pfl = eq_aug.get_profile('PFL')                # poloidal flux label
 
     ds = xr.Dataset({
-        'psi': (['R', 'Z', 'time'], psi_RZ),  # poloidal flux matrix
-        'pressure' : (['psi_n', 'time'], pressure),
-        # pleque internally uses derivatives with respect to psi_n and will later divide by this factor
-        'pprime' : (['psi_n', 'time'], pprime * psi_n_factor),
-        'FFprime' : (['psi_n', 'time'], ffprime * psi_n_factor),
-        'q' : (['psi_n', 'time'], q),
-        'pfl': (['psi_n'], pfl),  # for consistence
+        'psi': (['R', 'Z', 'time'], eq_aug.pfm),  # poloidal flux matrix
+        'pressure' : (['time', 'psi_n'], pressure),
+        'pprime' : (['time', 'psi_n'], pprime),
+        'FFprime' : (['time', 'psi_n'], FFprime),
+        'Fprime' : (['time', 'psi_n'], Fprime),
+        'q' : (['time', 'psi_n'], q),
+        'pfl': (['time', 'psi_n'], pfl),  # for consistence
+        'psi_lcfs': (['time'], eq_aug.psix),
+        'psi_axis': (['time'], eq_aug.psi0),
+        # TODO include also other hinting points like mag axis
     }, coords={
-        'time': time,
-        'Rt': (['R', 'time'], R),
-        'Zt': (['Z', 'time'], Z),
-        'psi_n': psi_n,
+        'time': eq_aug.t_eq,
+        # NOTE eq_aug.Rmesh is assumed to be constant and probably does not
+        # evolve, it typically contains many more time steps than actually used
+        'R': eq_aug.Rmesh,
+        'Z': eq_aug.Zmesh,
     })
-
+    ds['psi_n_factor'] = ds['psi_lcfs'] - ds['psi_axis']
+    ds.coords['psi_nt'] = (ds['pfl'] - ds['psi_axis']) / ds['psi_n_factor']
+    # pleque internally uses and expects derivatives with respect to psi_n and
+    # will later divide by psi_n_factor, so here it is multiplied in
+    ds = ds.update(ds[['pprime', 'Fprime', 'FFprime']] * ds['psi_n_factor'])
 
     # TODO is it worth doing sortby('time') if only sel is used?
-    eqs = EquilibriaTimeSlices(ds)
+    # Yes, if interpolating
+    eqs = EquilibriaTimeSlices(ds, None)
 
     if time is not None:
         return eqs.get_time_slice(time)
