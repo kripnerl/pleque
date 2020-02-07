771ee737e8dcb7d6931a508bc7a08b7ea3b3904d
Lukas Kripner <kripner@ipp.cas.cz>
2020-02-07 13:35:37 +0100

    remove nbsphinx from packages

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,7 +38,6 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.mathjax',
               'sphinx.ext.viewcode',
               'sphinx.ext.githubpages',
-              'nbsphinx',
               ]
 
 # Add any paths that contain templates here, relative to this directory.
